1747aa88a0e626794bb2c079677e6961233098dd
yogendra dhami <yogendradhami6@gmail.com>
2023-03-22 10:30:48 +0545

    password change page has been created successfully

--- hospital_management_system/authentication/urls.py
+++ hospital_management_system/authentication/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from .views import LoginPage, RegisterPage, LogoutPage
+from .views import LoginPage, RegisterPage, LogoutPage,PasswordsChangeView
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns =[
     path('login/', LoginPage.as_view(), name='user_login'),
     path('logout/',LogoutPage.as_view(), name= 'logout'),
     path('register/', RegisterPage.as_view(), name='user_registration'),
     path('dashboard', views.Dashboard, name='dashboard' ),
-    path('change-password/<token>/', views.ChangePassword, name='change-password'),
-    path('forgot-password/', views.forgetPassword, name='forgot-password'),
+
+    # path('change-password/<token>/', views.ChangePassword, name='change-password'),
+
+    # path('forgot-password/', views.forgetPassword, name='forgot-password'),
+
+
+    # path('password/',auth_views.PasswordChangeView.as_view(template_name='authentication/change-password.html' )),
+    path('change-password/', PasswordsChangeView.as_view(template_name='authentication/change-password.html'),name='change-password'),
 ]
--- hospital_management_system/authentication/views.py
+++ hospital_management_system/authentication/views.py
@@ -9,6 +9,10 @@ from django.core.mail import send_mail
 from .helpers import send_forget_password_mail
 import uuid
 from .models import Profile
+from django.contrib.auth.views import PasswordChangeView
+from django.contrib.auth.forms import PasswordChangeForm
+from django.urls import reverse_lazy
+from .form import PasswordChangingForm
 
 
 # Create your views here.
@@ -72,39 +76,66 @@ class RegisterPage(View):
             return redirect('user_login')
         
 
-def ChangePassword(request,token):
-    context={}
-    try:
-        profile_obj =Profile.objects.filter(forget_password_token =token).first()
-
-        print(profile_obj)
-
-
-    except Exception as e:
-        print(e)
-    return render(request, 'authentication/change_password.html')
-
-def forgetPassword(request):
-    try:
-        if request.method == 'POST':
-            username=request.POST.get('username')
-
-            if not User.objects.filter(username=username).first():
-                messages.success(request, 'No user found with this username.')
-                return redirect('forgot-password')
-            
-            user_obj= User.objects.get(username=username)
-            token =str(uuid.uuid4())
-            profile_obj = Profile.objects.get(user=user_obj)
-            profile_obj.forgot_password_token=token
-            
-            send_forget_password_mail(user_obj, token)
-            messages.success(request, 'An email is sent.')
-            return redirect('forgot-password')
-
-
-    except Exception as e:
-        print(e)
-    return render(request, 'authentication/forgot_password.html')
-
+# def ChangePassword(request,token):
+#     context={}
+#     try:
+#         profile_obj =Profile.objects.filter(forget_password_token =token).first()
+#         context  = {'user_id': profile_obj.user.id}
         
+#         if request.method=='POST':
+#             new_password=request.POST.get('new_password')
+#             confirm_password=request.POST.get('reconfirm_password')
+#             user_id=request.POST.get('user_id')
+
+#             if user_id is not None:
+#                 messages.success(request, "NO user id found.")
+#                 return render(f'change-password{token}')
+            
+
+#             if new_password != confirm_password:
+#                 messages.success(request, "Both should be equal.")
+#                 return render(f'change-password{token}')
+            
+#             user_obj =User.objects.get(id=user_id)
+#             user_obj.set_password(new_password)
+#             user_obj.save()
+#             return redirect('user-login')
+
+#         context  = {'user_id': profile_obj.user.id}
+
+#     except Exception as e:
+#         print(e)
+#     return render(request, 'authentication/change_password.html',context)
+
+# def forgetPassword(request):
+#     try:
+#         if request.method == 'POST':
+#             username=request.POST.get('username')
+
+#             if not User.objects.filter(username=username).first():
+#                 messages.success(request, 'No user found with this username.')
+#                 return redirect('forgot-password')
+            
+#             user_obj= User.objects.get(username=username)
+#             token =str(uuid.uuid4())
+#             profile_obj = Profile.objects.get(user=user_obj)
+#             profile_obj.forgot_password_token=token
+#             profile_obj.save()
+#             send_forget_password_mail(user_obj, token)
+#             messages.success(request, 'An email is sent.')
+#             return redirect('forgot-password')
+
+
+#     except Exception as e:
+#         print(e)
+#     return render(request, 'authentication/forgot_password.html')
+
+
+# change password
+class PasswordsChangeView(PasswordChangeView):
+    form_class  = PasswordChangingForm
+    # from_class= PasswordChangeForm
+    # messages.success(request, "Your Password has been Changed Successfully!!!")
+    success_url=reverse_lazy('user_login')
+
+
